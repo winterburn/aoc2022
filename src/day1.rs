393b27d591b62137e0c2eeb9a2b52ee2d544d05f
Olli Helminen <o.helminen@proton.me>
2023-03-25 09:47:41 +0200

    running Rustfmt on both files.

--- src/day1.rs
+++ src/day1.rs
@@ -1,12 +1,15 @@
-
 #[aoc_generator(day1)]
 pub fn input_generator(input: &str) -> Vec<i32> {
     let mut elf_calories: Vec<i32> = vec![];
     let mut total_calories: i32 = 0;
-    for calory in input.lines()  {
+    for calory in input.lines() {
         let calory: i32 = match calory.trim().parse() {
-            Ok(value) => {value},
-            Err(_) => {elf_calories.push(total_calories); total_calories = 0; continue},
+            Ok(value) => value,
+            Err(_) => {
+                elf_calories.push(total_calories);
+                total_calories = 0;
+                continue;
+            }
         };
         total_calories = total_calories + calory;
     }
@@ -14,26 +17,21 @@ pub fn input_generator(input: &str) -> Vec<i32> {
     elf_calories
 }
 
-
-
 #[aoc(day1, part1)]
 pub fn solve_part1(input: &[i32]) -> i32 {
-
     let max_value = input.iter().max();
 
     match max_value {
-        Some(max) => {return *max}
-        None => {return 0}
+        Some(max) => return *max,
+        None => return 0,
     }
 }
 
 #[aoc(day1, part2)]
 pub fn solve_part2(input: &Vec<i32>) -> i32 {
-
     let mut sorted = input.clone();
 
-    sorted.sort_by(|a,b| b.cmp(a));
-
+    sorted.sort_by(|a, b| b.cmp(a));
 
     sorted[0] + sorted[1] + sorted[2]
 }
--- src/day2.rs
+++ src/day2.rs
@@ -6,23 +6,45 @@ pub struct Round {
 impl Round {
     fn points(&self) -> u32 {
         let move_point = self.you.value();
-        if self.you == self.elf {return move_point + 3}
-        if self.you == Move::Rock && self.elf == Move::Scissors {return move_point + 6}
-        if self.you == Move::Paper && self.elf == Move::Rock {return move_point + 6}
-        if self.you == Move::Scissors && self.elf == Move::Paper {return move_point + 6}
+        if self.you == self.elf {
+            return move_point + 3;
+        }
+        if self.you == Move::Rock && self.elf == Move::Scissors {
+            return move_point + 6;
+        }
+        if self.you == Move::Paper && self.elf == Move::Rock {
+            return move_point + 6;
+        }
+        if self.you == Move::Scissors && self.elf == Move::Paper {
+            return move_point + 6;
+        }
         move_point
     }
     fn points_round2(&self) -> u32 {
-        if self.you == Move::Paper {return self.elf.value() + 3}
+        if self.you == Move::Paper {
+            return self.elf.value() + 3;
+        }
         if self.you == Move::Rock {
-            if self.elf == Move::Paper {return 1}
-            if self.elf == Move::Scissors {return 2}
-            if self.elf == Move::Rock {return 3}
+            if self.elf == Move::Paper {
+                return 1;
+            }
+            if self.elf == Move::Scissors {
+                return 2;
+            }
+            if self.elf == Move::Rock {
+                return 3;
+            }
         }
         if self.you == Move::Scissors {
-            if self.elf == Move::Paper {return 9}
-            if self.elf == Move::Scissors {return 7}
-            if self.elf == Move::Rock {return 8}
+            if self.elf == Move::Paper {
+                return 9;
+            }
+            if self.elf == Move::Scissors {
+                return 7;
+            }
+            if self.elf == Move::Rock {
+                return 8;
+            }
         }
         0
     }
@@ -32,14 +54,14 @@ impl Round {
 enum Move {
     Rock,
     Paper,
-    Scissors
+    Scissors,
 }
 
 #[derive(Debug)]
 struct InputError;
 
 impl Move {
-    fn from_str(s: &str,) -> Result<Move, InputError> {
+    fn from_str(s: &str) -> Result<Move, InputError> {
         match s {
             "A" => Ok(Move::Rock),
             "B" => Ok(Move::Paper),
@@ -47,18 +69,17 @@ impl Move {
             "Y" => Ok(Move::Paper),
             "X" => Ok(Move::Rock),
             "Z" => Ok(Move::Scissors),
-            _ => Err(InputError)
+            _ => Err(InputError),
         }
     }
     fn value(&self) -> u32 {
         match self {
             Move::Rock => return 1,
             Move::Paper => return 2,
-            Move::Scissors=> return 3,
+            Move::Scissors => return 3,
         }
-    } 
+    }
 }
-
 
 #[aoc_generator(day2)]
 pub fn input_generator(input: &str) -> Vec<Round> {
@@ -70,8 +91,8 @@ pub fn input_generator(input: &str) -> Vec<Round> {
                 elf: Move::from_str(round.next().unwrap()).unwrap(),
                 you: Move::from_str(round.next().unwrap()).unwrap(),
             }
-        }).collect()
-    
+        })
+        .collect()
 }
 
 #[aoc(day2, part1)]
